e2296d57bb952d55ba2f522f7e6ac3120da5e5f5
Mohamed essamaali <mohamed.essamaali@gmail.com>
2020-10-08 23:28:13 -0400

    fixed the errors

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -16,7 +16,7 @@ const ContactForm = () => {
         <div>
           <label htmlFor="firstName">First Name*</label>
           <input
-           id='firstName'
+          
             data-testid = 'input-fname'
             name="firstName"
             placeholder="Edd"
@@ -30,7 +30,7 @@ const ContactForm = () => {
         <div>
           <label htmlFor="lastName">Last Name*</label>
           <input
-          id='lastName'
+         
           data-testid = 'input-lname'
             name="lastName"
             placeholder="Burke"
@@ -45,14 +45,14 @@ const ContactForm = () => {
           <label htmlFor="email" placeholder="bluebill1049@hotmail.com">
             Email*
           </label>
-          <input id='email' name="email" ref={register({ required: true })} data-testid = 'input-email'/>
+          <input name="email" ref={register({ required: true })} data-testid = 'input-email'/>
           {errors.email && (
             <p>Looks like there was an error: {errors.email.type}</p>
           )}
         </div>
         <div>
           <label htmlFor="message">Message</label>
-          <textarea id='message' name="message" ref={register({ required: false })} data-testid = 'input-message' />
+          <textarea  name="message" ref={register({ required: false })} data-testid = 'input-message' />
         </div>
         {data && (
           <pre style={{ textAlign: "left", color: "white" }}>
--- src/components/ContactForm.test.js
+++ src/components/ContactForm.test.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { render, fireEvent, screen } from "@testing-library/react";
 import ContactForm from "./ContactForm";
+import { act } from 'react-dom/test-utils';
 
 
 test('renders ContactForm without errors', () => {
@@ -12,17 +13,17 @@ test('test user input and submit button', async ()=>{
     render(<ContactForm />);
 
     //query the virtual dom
-    const  firstName = screen.getByTestId(/input-fname/i);
-    const  lastName = screen.getByTestId(/input-lname/i);
-    const  email = screen.getByTestId(/input-email/i);
-    const  message = screen.getByTestId(/input-message/i);
+    const  firstName = screen.getByPlaceholderText(/edd/i);
+    const  lastName = screen.getByPlaceholderText(/burke/i);
+    const  email = screen.getByTestId("input-email");
+    const  message = screen.getByTestId('input-message');
 
     //fill out the form
 
-    fireEvent.change(firstName, {target:{value:"Edd"}});
-    fireEvent.change(lastName, {target:{value: "Burke"}});
-    fireEvent.change(email, {target:{value:"bluebill1049@hotmail.com"}});
-    fireEvent.change(message, {target:{value:"I am testing this form"}});
+    fireEvent.change(firstName, {target: {value:"Edd"}});
+    fireEvent.change(lastName, {target: {value: "Burke"}});
+    fireEvent.change(email, {target: {value:"bluebill1049@hotmail.com"}});
+    fireEvent.change(message, {target: {value:"I am testing this form"}});
      
     //check the assertion that the inputs contain the values we typed in
     expect(firstName).toHaveValue('Edd')
@@ -30,8 +31,14 @@ test('test user input and submit button', async ()=>{
     expect(email).toHaveValue('bluebill1049@hotmail.com');
     expect(message).toHaveValue('I am testing this form');
      // Submit the form (Careful -- state changes can happen asynchronously)
-     const button = screen.getByTestId(/submit-button/i);
-     fireEvent.click(button);
+
+    await act(async ()=>{
+        const button = screen.getByTestId(/submit-button/i);
+        fireEvent.click(button);
+    })
+       
+    
+     
 
     
 })
